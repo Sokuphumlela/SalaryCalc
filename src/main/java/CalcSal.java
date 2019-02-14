a057d3dfd5e9ea787ba8d7313d6768e0ab75bce9
Lungisa Lucky Mkhasakhasa <47332050+Luuyhisa@users.noreply.github.com>
2019-02-14 06:02:10 +0200

    Update CalcSal.java

--- src/main/java/CalcSal.java
+++ src/main/java/CalcSal.java
@@ -1,8 +1,10 @@
 public class CalcSal {
-
+//calculating Salarries
     public int Salary(int intHours,int intRate){
         int intSalary = intRate*intHours;
         return intSalary;
     }
 
+    
+    
 }
